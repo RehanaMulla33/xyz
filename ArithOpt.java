6ff2399098b15da9ca3ef5181974f6599f5be6ea
RehanaMulla33 <89864443+RehanaMulla33@users.noreply.github.com>
2021-11-15 11:45:45 +0530

    Add files via upload

---
+++ ArithOpt.java
@@ -0,0 +1,17 @@
+class ArithOpt{
+	public static void main(String args[]){
+		int num1=40;
+		
+		int num2=20;
+		
+		
+		int result=num1+num2;
+		int result1=num1-num2;
+		int result2=num1*num2;
+		int result3=num1/num2;
+		System.out.println("addition of" +num1+ "and" +num2+ " is ="+result);
+		System.out.println("Substraction of " +num1+ "and" +num2+ " is ="+result1);
+		System.out.println("Multiplication of" +num1+ "and " +num2+ "is ="+result2);
+		System.out.println("Division of" +num1+ "and " +num2+ "is ="+result3);
+		}
+		}
